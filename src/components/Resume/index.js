17c6257de8788f75df91c37dad43358599774dd8
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-09-06 21:42:34 -0700

    fixed some minor bugs

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,20 +9,25 @@ function Resume() {
                 <h1 className="title">Resume</h1>
                 <hr />
                 <Row>
-                    <Col>
-                    <div class="justify-content-center mt-5">
-                        <div id="front-back">
-                            <h2>Front End Experience</h2>
-                            <p>HTML, CSS (Bootstrap, Foundation, Materialize), JavaScript, jQuery, React.js</p>
+                    <Col md="12">
+                        <div class="justify-content-center mt-5">
+                            <div id="front-back">
+                                <h2>Front End Experience</h2>
+                                <p>HTML, CSS (Bootstrap, Foundation, Materialize), JavaScript, jQuery, React.js</p>
+                            </div>
+                    
+                            <div id="front-back" class="mt-5">
+                                <h2>Back End Experience</h2>
+                                <p>
+                                    Node.js, Express.js, mySQL, Sequelize, NoSQL (MongoDB, Mongoose), Python, API's (third-party, RESTful, server-side), Templating (Handlebars)
+                                </p>
+                            </div>
                         </div>
-                
-                        <div id="front-back" class="mt-5">
-                            <h2>Back End Experience</h2>
-                            <p>
-                                Node.js, Express.js, mySQL, Sequelize, NoSQL (MongoDB, Mongoose), Python, API's (third-party, RESTful, server-side), Templating (Handlebars)
-                            </p>
+                    </Col>
+                    <Col> 
+                        <div class="justify-content-center mt-5">
+                            <p className="text-center">Donload my full <a href="https://www.linkedin.com/in/gustavo-muratalla/" style={{ color: '#003049' }} className="link text-decoration-none">resume</a> (PDF Comming Soon!)</p>
                         </div>
-                    </div>
                     </Col>
                 </Row>
             </Container>
